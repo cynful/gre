cda5f57a31ab0b491f6fa2fceaaadb18e4e0277c
cynful <cynthia.m.kwok@gmail.com>
2019-01-13 21:08:51 -0800

    more succinct, fewer lines

--- quizme.py
+++ quizme.py
@@ -3,17 +3,15 @@
 import sys
 
 def readinquiz(filename):
-    f = open(filename)
-    for line in f:
-        l = line.strip()
-        if l == "Q." or l == "A." or l == "S.":
-            infolist = list()
-            while line != '\n':
-                infolist.append(line.strip())
-                line = f.readline()
-            print(infolist)
-    f.close()
-    return
+    with open(filename) as f:
+        for line in f:
+            l = line.strip()
+            if l == "Q." or l == "A." or l == "S.":
+                infolist = list()
+                while line != '\n':
+                    infolist.append(line.strip())
+                    line = f.readline()
+                print(infolist)
 
 def main():    
     arg = sys.argv[1]
